35dcd349b368922280560d6f96fa15e508c0490d
bintadam <zakiyaadan12@gmail.com>
2023-07-14 15:40:16 +0200

    add array

---
+++ script.js
@@ -0,0 +1,6 @@
+const arr =[
+    {firstname:"Ahmed", lastname:"Abdi", country:"Germany", score: 90,},
+    {firstname:"Jeniifer", lastname:"Wambui", country:"kenya", score: 76,},
+    {firstname:"Deema", lastname:"Mohamed", country:"Palestine", score: 85,},
+    {firstname:"Mathew", lastname:"John", country:"France",score: 64,}
+]
