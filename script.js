c9c4b29e7601e6e8fcf9de7bfeda6d92dd4d3adb
bintadam <zakiyaadan12@gmail.com>
2023-07-14 16:36:09 +0200

    add eventlistener

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const arr =[
-    {firstname:"Ahmed", lastname:"Abdi", country:"Germany", score: 90,},
-    {firstname:"Jeniifer", lastname:"Wambui", country:"kenya", score: 76,},
-    {firstname:"Deema", lastname:"Mohamed", country:"Palestine", score: 85,},
-    {firstname:"Mathew", lastname:"John", country:"France",score: 64,}
-]
-
-
-
 $(document).ready(function(){
-  const firstname = $('#firstname').value
-  const lastname = $('#lastname').value  
-  const country= $('#country').value
-  const score = $('#score').value      
+    const arr =[
+        {firstname:"Ahmed", lastname:"Abdi", country:"Germany", score: 90,},
+        {firstname:"Jenifer", lastname:"Wambui", country:"kenya", score: 76,},
+        {firstname:"Deema", lastname:"Mohamed", country:"Palestine", score: 85,},
+        {firstname:"Mathew", lastname:"John", country:"France",score: 64,}
+    ]
+
+    const btn = $('.btn')
+    btn.click(function(e){
+        e.preventDefault()
+
+        const main = $('.main')
+        const firstname = $('#firstname').val()
+        const lastname = $('#lastname').val()  
+        const country= $('#country').val()
+        const score = parseInt($('#score').val())
+
+        const newObj = {
+            firstname: firstname,
+            lastname: lastname,
+            country: country,
+            score: score
+        }
+
+        arr.push(newObj)
+        console.log(arr)
+    
+        const html = `
+            <div class="card">
+                <p>${firstname} ${lastname}</p>
+                <p>${country}</p>
+                <p>${score}</p>
+            </div>`
+
+        $(main).append(html)
+
+  })      
 })
